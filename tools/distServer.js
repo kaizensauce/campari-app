5314df7615b43bc7e6f0638fe15e14d8118df0f3
kaizensauce <kaizensauce@gmail.com>
2016-10-10 08:34:43 +0100

    Updated title

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,10 +1,3 @@
-// This file configures a web server for testing the production build
-// on your local machine.
-
-// import browserSync from 'browser-sync';
-// import historyApiFallback from 'connect-history-api-fallback';
-
-
 import express from 'express';
 var app = express();
 
@@ -14,20 +7,3 @@ app.use(express.static('dist'));
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
-
-// // Run Browsersync
-// browserSync({
-//   port: port,
-//   ui: {
-//     port: 3001
-//   },
-//   server: {
-//     baseDir: 'dist'
-//   },
-
-//   files: [
-//     'src/*.html'
-//   ],
-
-//   middleware: [historyApiFallback()]
-// });
