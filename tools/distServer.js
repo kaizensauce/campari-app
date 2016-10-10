ac4231d08a0d4f1aa1adeaee13e4b07ceab37e2e
kaizensauce <kaizensauce@gmail.com>
2016-10-10 06:13:15 +0100

    change to use express for dist server

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,23 +1,33 @@
 // This file configures a web server for testing the production build
 // on your local machine.
 
-import browserSync from 'browser-sync';
-import historyApiFallback from 'connect-history-api-fallback';
+// import browserSync from 'browser-sync';
+// import historyApiFallback from 'connect-history-api-fallback';
+
+
+import express from 'express';
+var app = express();
+
 var port = (process.env.PORT || 3000);
+app.use(express.static('dist'));
 
-// Run Browsersync
-browserSync({
-  port: port,
-  ui: {
-    port: 3001
-  },
-  server: {
-    baseDir: 'dist'
-  },
-
-  files: [
-    'src/*.html'
-  ],
-
-  middleware: [historyApiFallback()]
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
 });
+
+// // Run Browsersync
+// browserSync({
+//   port: port,
+//   ui: {
+//     port: 3001
+//   },
+//   server: {
+//     baseDir: 'dist'
+//   },
+
+//   files: [
+//     'src/*.html'
+//   ],
+
+//   middleware: [historyApiFallback()]
+// });
